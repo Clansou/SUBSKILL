e69b61548ebedd687d8ddd142ab52607807eb58e
unknown <clement.souplet78@gmail.com>
2023-08-22 13:12:41 +0200

    add Fixtures Jobs

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Cities;
+use App\Entity\Jobs;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -15,6 +16,11 @@ class AppFixtures extends Fixture
             $city = new Cities();
             $city->setCityName($name);
             $manager->persist($city);
+        }
+        foreach(["CDI","CDD","Alternance","Stage","Freelance","Temps partiel"] as $jobsname){
+            $jobs = new Jobs();
+            $jobs->setJobName($jobsname);
+            $manager->persist($jobs);
         }
 
         $manager->flush();
