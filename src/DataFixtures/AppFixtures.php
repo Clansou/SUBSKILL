ca852305671f614ebbcdc49cd88102ad834fb27a
unknown <clement.souplet78@gmail.com>
2023-08-22 13:46:25 +0200

    add Fixtures JobsAnnoncements

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\Cities;
 use App\Entity\Contracts;
 use App\Entity\Jobs;
 use App\Entity\Companies;
+use App\Entity\JobAnnouncements;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -36,6 +37,41 @@ class AppFixtures extends Fixture
         }
         
 
+        $manager->flush();
+
+        $citiesRepository = $manager->getRepository(Cities::class);
+        $cities = $citiesRepository->findAll();
+
+        $contractsRepository = $manager->getRepository(Contracts::class);
+        $contracts = $contractsRepository->findAll();
+
+        $jobsRepository = $manager->getRepository(Jobs::class);
+        $jobs = $jobsRepository->findAll();
+
+        $companiesRepository = $manager->getRepository(Companies::class);
+        $companies = $companiesRepository->findAll();
+
+        for ($i=1; $i<=50;$i++){
+            $randKeyCities = rand(0, count($cities) -1);
+            $randKeyContracts = rand(0, count($contracts) -1);
+            $randKeyJobs = rand(0, count($jobs) -1);
+            $randKeyCompanies = rand(0, count($companies) -1);
+
+            $jobAnnouncement = new JobAnnouncements();
+            $jobAnnouncement->setPublishDate(new \DateTime('01/09/2023'));
+            $jobAnnouncement->setLastUpdateDate(new \DateTime('01/09/2023'));
+            $jobAnnouncement->setAnnouncementsTitle("Announcements Title");
+            $jobAnnouncement->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ");
+
+            $jobAnnouncement->setCity($cities[$randKeyCities]);
+            $jobAnnouncement->setContract($contracts[$randKeyContracts]);
+            $jobAnnouncement->setJob($jobs[$randKeyJobs]);
+            $jobAnnouncement->setCompany($companies[$randKeyCompanies]);
+
+            $manager->persist($jobAnnouncement);
+
+
+        }
         $manager->flush();
     }
 }
