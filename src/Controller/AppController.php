2ad1849d07f1c5a59d3275ce6a000522159e40ab
unknown <clement.souplet78@gmail.com>
2023-08-22 17:19:50 +0200

    Add pagination

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -6,14 +6,23 @@ use App\Repository\JobAnnouncementsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_app')]
-    public function index(JobAnnouncementsRepository $jobAnnouncements): Response
+    public function index(JobAnnouncementsRepository $jobAnnouncements,Request $request): Response
     {
+        $pageNumber = $request->query->getInt('pages', 1);
+        $perPage = 10;
+        $totalJobAnnouncements = $jobAnnouncements->count([]);
+        $maxPage = ceil($totalJobAnnouncements / $perPage);
+
+        $jobAnnouncements = $jobAnnouncements->findBy([], [], $perPage, ($pageNumber-1)*10);
         return $this->render('app/index.html.twig', [
-            'jobAnnouncements' => $jobAnnouncements->findAll(),
+            'jobAnnouncements' =>  $jobAnnouncements,
+            'pages' => $pageNumber,
+            'maxPage' => $maxPage,
         ]);
     }
 }
